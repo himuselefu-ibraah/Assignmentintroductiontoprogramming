2a34d485037744729c62710dabd1795f265cc049
Ibrahim Himuselefu <ibrahimchristopher004@gmail.com>
2025-10-03 21:43:45 +0300

    Update and rename Unitsforsemone.cxx to Unitsforsemone.c

---
+++ Unitsforsemone.c
@@ -0,0 +1,19 @@
+/*
+Christopher Nyasimba 
+PA106/G/29043/25
+This is a program to display all units in our first semester
+
+*/
+
+#include<stdio.h> //to access printf
+int main() {
+    printf("Software Engineering Units\n");
+    printf("1.Computer systems and organization\n");
+    printf("2.Introduction to programming and problem solving.\n");
+    printf("3.Fundamentals of sytems analysis and design\n");
+    printf("4.Mathematics for science\n");
+    printf("5.Discrete Mathematics\n");
+    printf("6.Fundamentals of ICT\n");
+    printf("7.Communication Skills\n");
+    return 0;
+}
