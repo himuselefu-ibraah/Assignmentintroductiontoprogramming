82f676b97cd3f371a71bd9bd6df312cac3a48140
Ibrahim Himuselefu <ibrahimchristopher004@gmail.com>
2025-10-10 13:23:26 +0300

    Update numbersinforloop.c

---
+++ numbersinforloop.c
@@ -0,0 +1,17 @@
+/*
+Chiristopher Nyasimba
+PA106/G/29043/25
+The program prints numbers from 100 downwards to 50.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i=100;i>=50;i--){
+        
+        printf("%d\n",i);
+    }
+    return 0;
+     
+}
